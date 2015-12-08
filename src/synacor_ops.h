c24e5ad8aa6dc8abbd0b58121c073877e26c947e
Brent Chesny <brent.chesny@gmail.com>
2015-12-08 14:14:58 +0100

    Added definitions of opcodes and big-ass switch

--- src/synacor_ops.h
+++ src/synacor_ops.h
@@ -6,9 +6,36 @@
 #define MAX_OP 21
 
 /**
+ * Definition of different opcodes
+ */
+#define OP_HALT 0
+#define OP_SET 1
+#define OP_PUSH 2
+#define OP_POP 3
+#define OP_EQ 4
+#define OP_GT 5
+#define OP_JMP 6
+#define OP_JT 7
+#define OP_JF 8
+#define OP_ADD 9
+#define OP_MULT 10
+#define OP_MOD 11
+#define OP_AND 12
+#define OP_OR 13
+#define OP_NOT 14
+#define OP_RMEM 15
+#define OP_WMEM 16
+#define OP_CALL 17
+#define OP_RET 18
+#define OP_OUT 19
+#define OP_IN 20
+#define OP_NOOP 21
+
+/**
  * Lookup table for the number of arguments for each opcode
  */
 extern uint8_t synacor_op_args[];
+
 
 
 #endif /* end of include guard: SYNACOR_OPS_H__ */
--- src/synacor_vm.c
+++ src/synacor_vm.c
@@ -25,17 +25,59 @@ int synacor_vm_run(synacor_vm* vm)
 int synacor_vm_step(synacor_vm* vm)
 {
 	uint16_t opcode = vm->memory[vm->pc];
-
-	if (opcode > MAX_OP)
-	{
-		printf("Error: Unrecognized opcode!\n");
-		synacor_vm_kill(vm);
-		return 1;
-	}
-
 	uint8_t argc = synacor_op_args[opcode];
 
-	// TODO execute op
+	switch(opcode) {
+		case OP_HALT:
+			break;
+		case OP_HALT:
+			break;
+		case OP_SET:
+			break;
+		case OP_PUSH:
+			break;
+		case OP_POP:
+			break;
+		case OP_EQ:
+			break;
+		case OP_GT:
+			break;
+		case OP_JMP:
+			break;
+		case OP_JT:
+			break;
+		case OP_JF:
+			break;
+		case OP_ADD:
+			break;
+		case OP_MULT:
+			break;
+		case OP_MOD:
+			break;
+		case OP_AND:
+			break;
+		case OP_OR:
+			break;
+		case OP_NOT:
+			break;
+		case OP_RMEM:
+			break;
+		case OP_WMEM:
+			break;
+		case OP_CALL:
+			break;
+		case OP_RET:
+			break;
+		case OP_OUT:
+			break;
+		case OP_IN:
+			break;
+		case OP_NOOP:
+		default:
+			printf("Error: Unrecognized opcode!\n");
+			synacor_vm_kill(vm);
+			return 1;
+	}
 
 	vm->pc++;
 
